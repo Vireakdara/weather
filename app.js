1ef8fae1927e3c284928387dcb24619421e7288f
Ly Vireakdara <lyvireakdara@gmail.com>
2021-06-21 14:09:43 +0700

    Done

--- app.js
+++ app.js
@@ -7,35 +7,31 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
-
-    // res.send("This server is currently is runnning");
 });
 
 app.post("/", (req, res) => {
-    console.log(req.body.cityName);
-    console.log("Post request recieved.");
+    // console.log(req.body.cityName);
+    // console.log("Post request has recieved.");
+    const city = req.body.cityName;
+    const apiKey = "72d8a7df3d2cfa42e98082f3e3d5b5dd";
+    const units = "metric"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=" + units + "&appid=" + apiKey + "";
+    const weatherIcon = "http://openweathermap.org/img/wn/";
+
+    https.get(url, (response) => { // console.log(response);
+        response.on("data", (d) => {
+            const data = JSON.parse(d);
+            const weather = data.weather[0].description;
+            const icon = data.weather[0].icon
+            const temp = data.main.temp;
+            res.write("<p>The Weather is currently " + weather + "</p>");
+            res.write("<h1>The temperatur in " + city + " is " + temp + " degress Celcius </h1>");
+            res.write("<img src=" + weatherIcon + icon + "@2x.png>");
+            res.send();
+        });
+    });
+
 })
-
-
-const city = "phnom penh";
-const apiKey = "72d8a7df3d2cfa42e98082f3e3d5b5dd";
-const units = "Metric"
-const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=" + units + "&appid=" + apiKey + "";
-const weatherIcon = "http://openweathermap.org/img/wn/03d@2x.png";
-
-
-// https.get(url, (response) => { // console.log(response);
-//     response.on("data", (d) => {
-//         const data = JSON.parse(d);
-//         const weather = data.weather[0].description;
-//         const temp = data.main.temp;
-//         res.write("<p>The Weather is currently " + weather + "</p>");
-//         res.write("<h1>The temperatur in Phnom Penh is " + temp + " degress Celcius </h1>");
-//         res.write("<img src=" + weatherIcon + ">");
-//         res.send();
-//     });
-// });
-
 
 app.listen(3000, () => {
     console.log("This server is running on port 3000");
